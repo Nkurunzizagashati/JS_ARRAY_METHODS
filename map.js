fdce9c7db03799e129583f5c84217d9576c250e2
Fabrice <f.nkurunziz@alustudent.com>
2024-09-01 12:42:43 +0200

    map method

--- map.js
+++ map.js
@@ -6,4 +6,11 @@ const newArray = array.map((item, index) => {
 	console.log(item);
 });
 
-console.log(newArray);
+console.log(newArray); // this will log an array with five undefined items since we didn't return anything in the map method
+// [undefined, undefined, undefined, undefined, undefined]
+
+const newArray2 = array.map((item, index) => {
+	return item * 2;
+});
+
+console.log(newArray2); // this will log [2, 4, 6, 8, 10]
